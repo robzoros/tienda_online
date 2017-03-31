a2494ff29f53551e1da733e1a6dd0ddddcf989be
robzoros <robzoros@gmail.com>
2017-03-31 10:41:47 +0200

    Bug

--- utiles.py
+++ utiles.py
@@ -16,7 +16,7 @@ end = datetime.strptime('4/1/2017 7:30 PM', '%m/%d/%Y %I:%M %p')
 # Producto cartesiano de tuplas para salvar productos relacionados
 # Función que rellena una lista de productos y luego llama a la función productoCartesiano para obtener la lista definitiva.
 def tuplas_producto(session, factura):
-  productos = session.execute("SELECT codigo_referencia FROM compras_detalle WHERE factura = '" + factura.factura + "'")
+  productos = session.execute("SELECT codigo_referencia FROM pedidos WHERE factura = '" + factura.factura + "'")
   lista_prod = []
   for producto in productos:
     lista_prod.append(producto.codigo_referencia)
